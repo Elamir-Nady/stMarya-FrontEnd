21b54bf6fde05abab3f76e6bdb318deffd06e913
Elamir_Nady <elamirnady@gmail.com>
2022-04-26 15:53:08 +0200

    edit files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# SARAI
+# Ebtida2y
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.6.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/SARAI'),
+      dir: require('path').join(__dirname, './coverage/ebtida2y'),
       subdir: '.',
       reporters: [
         { type: 'html' },
